b658899bcd0fa83cc3c3559f7ab5974300b103ad
Natalia <natalia.contactod@gmail.com>
2023-01-30 11:05:17 -0500

    Subiendo actividad de logica

---
+++ practica1.js
@@ -0,0 +1,16 @@
+const dato1 = prompt("Ingrese primer numero");
+const dato2 = prompt("ingrese segundo numero"); 
+const dato3 = prompt("ingrese tercer numero");
+let numeros = [dato1,dato2,dato3];
+alert("Los numeros ingresados fueron" + numeros);
+
+if (dato1 == dato2 && dato2 == dato3)
+alert("los numeros son iguales")
+
+
+numeros.sort(); 
+    console.log(numeros);
+
+
+ numeros.reverse(); 
+    console.log(numeros);   
